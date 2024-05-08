9ca2e11adcf88ea40403296d9a44413210ea40fd
Avinash Pandey <avinash.12009084@lpu.in>
2024-05-08 12:32:37 +0530

    Reinstall of Vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,31 @@
-import { Container } from "@chakra-ui/react";
-import { Routes, Route } from "react-router-dom";
-import UserPage from "./pages/UserPage";
-import PostPage from "./pages/PostPage";
-// import Logo from "./components/logo/Logo";
-import Header from "./components/header/Header";
+import { useState } from "react";
 
-const App = () => {
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
-      <Container maxW={"620px"}>
-        <Header />
-        <Routes>
-          <Route path="/:username" element={<UserPage />} />
-          <Route path="/:username/post/:pid" element={<PostPage />} />
-        </Routes>
-      </Container>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
   );
-};
+}
 
 export default App;
--- client/src/pages/PostPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PostPage = () => {
-  return <div>PostPages</div>;
-};
-
-export default PostPage;
