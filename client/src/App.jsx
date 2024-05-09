1049b2fda07cfc1299fac3409a9ca4677936dfd2
Avinash Pandey <avinash.12009084@lpu.in>
2024-05-09 10:38:34 +0530

    Ui Design of Auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import HomePage from "./pages/home/HomePage";
+
 function App() {
   return (
     <>
-      <div className="flex max-w-6xl mx-auto">Hello MyX</div>
+      <div className="flex max-w-6xl mx-auto">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
