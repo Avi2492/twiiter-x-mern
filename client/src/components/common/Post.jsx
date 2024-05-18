ff751b15d9f42b16ac81daa84da73c1c901e7055
Avinash Pandey <avinash.12009084@lpu.in>
2024-05-18 14:13:49 +0530

    Comment Section Done

--- client/src/components/common/Post.jsx
+++ client/src/components/common/Post.jsx
@@ -10,11 +10,19 @@ import {
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
   const [comment, setComment] = useState("");
   const { data: authUser } = useQuery({ queryKey: ["authUser"] });
   const queryClient = useQueryClient();
+  const postOwner = post.user;
+  const isLiked = post.likes.includes(authUser._id);
+
+  const isMyPost = authUser._id === post.user._id;
+
+  const formattedDate = formatPostDate(post.createdAt);
+
   const { mutate: deletePost, isPending: isDeleting } = useMutation({
     mutationFn: async () => {
       try {
@@ -77,14 +85,39 @@ const Post = ({ post }) => {
     },
   });
 
-  const postOwner = post.user;
-  const isLiked = post.likes.includes(authUser._id);
+  const { mutate: commentPost, isPending: isCommenting } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`/api/v1/posts/comment/${post._id}`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ text: comment }),
+        });
 
-  const isMyPost = authUser._id === post.user._id;
+        const data = await res.json();
 
-  const formattedDate = "1h";
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
 
-  const isCommenting = false;
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+
+    onSuccess: () => {
+      toast.success("Commented Success!");
+
+      setComment("");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+    },
+    onError: () => {
+      toast.error("Commented Not Success!");
+    },
+  });
 
   const handleDeletePost = () => {
     deletePost();
@@ -92,6 +125,8 @@ const Post = ({ post }) => {
 
   const handlePostComment = (e) => {
     e.preventDefault();
+    if (isCommenting) return;
+    commentPost();
   };
 
   const handleLikePost = () => {
